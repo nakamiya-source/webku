f37ec975a4c7e7c26bcb080f0de5ec350b02b3a0
nakamiya-source <nakamiyanaaa@gmail.com>
2026-05-31 14:59:50 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,17 +1,16 @@
 document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
     
-    var usernameInput = document.getElementById('username').value;
-    var passwordInput = document.getElementById('password').value;
+    var usernameInput = document.getElementById('username').value.trim();
+    var passwordInput = document.getElementById('password').value.trim();
     var alertBox = document.getElementById('alertBox');
     
-    // Mengambil data pengguna yang tersimpan di localStorage dari pendaftaran
     var storedUser = localStorage.getItem('username');
     var storedPass = localStorage.getItem('password');
     
-    if (usernameInput === storedUser && passwordInput === storedPass) {
+    if (storedUser && usernameInput === storedUser && passwordInput === storedPass) {
         alert('Login Berhasil!');
-        window.location.href = '../index.html'; // Kembali ke landing page utama
+        window.location.href = "/webku/index.html";
     } else {
         alertBox.style.display = 'block';
         alertBox.innerText = 'Username atau Password salah!';
